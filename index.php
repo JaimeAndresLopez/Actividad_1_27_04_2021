f61b5c593d9036e354689c9ae19a0808f79c798a
JaimeAndresLopez <james_2005_scott@hotmail.coml>
2021-04-27 17:05:17 -0500

    Actividad 1 de php 27-04-21

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    $nombre = (string) "Jaime";
+    $apellido = (string) "Lopez";
+    $estatura = (float) 1.87;
+    $edad = (int) 27;
+    $genero = (string) "Masculino";
+    const Cedula = 12345678;
+    echo "Mi nombre es $nombre $apellido y tengo $edad años, mido $estatura mts y mi genero es $genero";
+    
+?>
